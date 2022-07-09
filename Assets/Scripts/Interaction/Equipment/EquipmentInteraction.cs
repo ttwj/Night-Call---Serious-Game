d496facfca05e08112540e9cbdcf06a42fb3ff33
Swee En <yapsweeen@hotmail.com>
2022-07-09 15:38:12 +0800

    ECG + part of IV drip

--- Assets/Scripts/Interaction/Equipment/EquipmentInteraction.cs
+++ Assets/Scripts/Interaction/Equipment/EquipmentInteraction.cs
@@ -2,14 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class EquipmentInteraction : MonoBehaviour
 {
     //Array of images to cater for different patient conditions, i.e. healthy / abnormal readings
     public Image[] PatientMonitorScreen;
-    public Image[] ECG;
+    public GameObject[] medicationVials;
     public Button closeButtonX;
+    public Text instructions;
+    public Animator anim;
     private Image activeImage;
+    private Vector3 cameraPos;
+    private GameObject target;
+    private string selectedObj;
+    public string ongoingTask;
+
+    //list of tasks:
+    //  [IVbagInjection, FinishedSelectingVial]
+
 
     void Start()
     {
@@ -19,6 +30,11 @@ public class EquipmentInteraction : MonoBehaviour
         }
 
         activeImage = null;
+
+        instructions.enabled = false;
+
+        anim = GameObject.Find("Syringe").GetComponent<Animator>();
+
         closeButtonX.gameObject.SetActive(false);
         closeButtonX.onClick.AddListener(closeImage);
     }
@@ -31,24 +47,132 @@ public class EquipmentInteraction : MonoBehaviour
 
             if (Physics.Raycast(ray, out hit, 100))
             {
+                if (hit.transform.gameObject.CompareTag("PatientMonitor")) { showPatientMonitorReadings(); }
 
-                //if player clicks on a patient monitor, show the corresponding image and a close button
-                //display image according to the patient's health, wait till later implementation
-                if (hit.transform.gameObject.CompareTag("PatientMonitor"))
+                if (hit.transform.gameObject.CompareTag("IV Stand"))
                 {
-                    PatientMonitorScreen[0].enabled = true;
-                    activeImage = PatientMonitorScreen[0];
-                    closeButtonX.gameObject.SetActive(true);
+                    IVbagInjection();
                 }
+
+                if (hit.transform.gameObject.CompareTag("Medication Vial") && ongoingTask == "IVbagInjection")
+                {
+                    selectedObj = hit.transform.gameObject.name;
+                    IVbagInjectionCont(selectedObj);
+                }
+
+                if (hit.transform.gameObject.name == "Syringe" && ongoingTask == "FinishedSelectingVial")
+                {
+                    anim.Play("SyringeAnim");
+                }
+
                 Debug.Log(hit.transform.gameObject.name);
             }
         }
     }
 
+    void showPatientMonitorReadings()
+    {
+        PatientMonitorScreen[0].enabled = true;
+        activeImage = PatientMonitorScreen[0];
+        closeButtonX.gameObject.SetActive(true);
+    }
+
     void closeImage()
-    {   
+    {
         closeButtonX.gameObject.SetActive(false);
         activeImage.enabled = false;
         activeImage = null;
+    }
+
+    void IVbagInjection()
+    {
+        cameraPos = Camera.main.transform.position;
+        target = GameObject.Find("MedicationVials");
+        changeCamPos(target);
+
+        instructions.text = "Choose the medication to inject into the IV drip bag.";
+        instructions.enabled = true;
+
+        ongoingTask = "IVbagInjection";
+    }
+
+    void IVbagInjectionCont(string selectedObj)
+    {
+        GameObject currentVial;
+
+        //remove all vials that were not selected, shift selected vial to center of table
+
+        if (selectedObj == "Medication Vial 1")
+        {
+            currentVial = medicationVials[0];
+
+            foreach (GameObject v in medicationVials[1..])
+            {
+                v.SetActive(false);
+            }
+            StartCoroutine(MoveFunction(currentVial));
+        }
+
+        if (selectedObj == "Medication Vial 2")
+        {
+            currentVial = medicationVials[1];
+
+            medicationVials[0].SetActive(false);
+            medicationVials[2].SetActive(false);
+            medicationVials[3].SetActive(false);
+            StartCoroutine(MoveFunction(currentVial));
+        }
+
+        if (selectedObj == "Medication Vial 3")
+        {
+            currentVial = medicationVials[2];
+
+            medicationVials[0].SetActive(false);
+            medicationVials[1].SetActive(false);
+            medicationVials[3].SetActive(false);
+            StartCoroutine(MoveFunction(currentVial));
+        }
+
+        if (selectedObj == "Medication Vial 4")
+        {
+            currentVial = medicationVials[3];
+
+            foreach (GameObject v in medicationVials[..3])
+            {
+                v.SetActive(false);
+            }
+            StartCoroutine(MoveFunction(currentVial));
+        }
+
+        ongoingTask = "FinishedSelectingVial";
+    }
+
+    IEnumerator MoveFunction(GameObject currentVial)
+    {
+        while (true)
+        {
+            Vector3 startPos = currentVial.transform.position;
+            Vector3 newPos = target.transform.position + new Vector3(0, -0.042f, 0.05f);
+
+            currentVial.transform.position = Vector3.Lerp(startPos, newPos, 2 * Time.deltaTime);
+            Debug.Log("I am enumerating");
+            Debug.Log("Current pos: " + currentVial.transform.position + ", New pos: " + newPos);
+
+            // If the object has arrived, stop the coroutine
+            if (currentVial.transform.position == newPos)
+            {
+                yield break;
+            }
+
+            // Otherwise, continue next frame
+            yield return null;
+        }
+    }
+
+    //to "switch scene"
+    void changeCamPos(GameObject target)
+    {
+        Camera.main.transform.position = target.transform.position + new Vector3(0.8f, 0.3f, 0);
+        Camera.main.transform.LookAt(target.transform);
     }
 }
