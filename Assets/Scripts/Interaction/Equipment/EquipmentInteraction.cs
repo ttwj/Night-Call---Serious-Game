9fda344ef02d73adfa46ea4be126b5a18e0f9b93
Swee En <yapsweeen@hotmail.com>
2022-07-09 09:59:12 +0800

    successfully show patient monitor reading on click

--- Assets/Scripts/Interaction/Equipment/EquipmentInteraction.cs
+++ Assets/Scripts/Interaction/Equipment/EquipmentInteraction.cs
@@ -8,33 +8,47 @@ public class EquipmentInteraction : MonoBehaviour
     //Array of images to cater for different patient conditions, i.e. healthy / abnormal readings
     public Image[] PatientMonitorScreen;
     public Image[] ECG;
+    public Button closeButtonX;
+    private Image activeImage;
 
-    // Start is called before the first frame update
     void Start()
     {
-        //Do not display any images on screen at the start of the game
         foreach (Image i in PatientMonitorScreen)
         {
             i.enabled = false;
         }
-    }
 
-    // Update is called once per frame
+        activeImage = null;
+        closeButtonX.gameObject.SetActive(false);
+        closeButtonX.onClick.AddListener(closeImage);
+    }
     void Update()
     {
-        if( Input.GetMouseButtonDown(0) )
-     {
-         Ray ray = Camera.main.ScreenPointToRay( Input.mousePosition );
-         RaycastHit hit;
-         
-         if( Physics.Raycast( ray, out hit, 100 ) )
-         {
-             if (hit.transform.gameObject.CompareTag("PatientMonitor"))
-             {
-                 PatientMonitorScreen[0].enabled = true;
-             }
-             Debug.Log( hit.transform.gameObject.name );
-         }
-     }
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 100))
+            {
+
+                //if player clicks on a patient monitor, show the corresponding image and a close button
+                //display image according to the patient's health, wait till later implementation
+                if (hit.transform.gameObject.CompareTag("PatientMonitor"))
+                {
+                    PatientMonitorScreen[0].enabled = true;
+                    activeImage = PatientMonitorScreen[0];
+                    closeButtonX.gameObject.SetActive(true);
+                }
+                Debug.Log(hit.transform.gameObject.name);
+            }
+        }
+    }
+
+    void closeImage()
+    {   
+        closeButtonX.gameObject.SetActive(false);
+        activeImage.enabled = false;
+        activeImage = null;
     }
 }
