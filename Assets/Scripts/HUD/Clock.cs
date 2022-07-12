d3e0d07db0887014c6b0c45b649dd2a4bd1bf842
Darryl Tan <darryl@Darryls-MacBook-Pro.local>
2022-07-12 23:00:16 +0800

    clock + energy countdown

---
+++ Assets/Scripts/HUD/Clock.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Clock : MonoBehaviour
+{
+    private TextMeshProUGUI textClock;
+    public int secondsToMinusHealth;
+    public bool minusHealth;
+    private float current;
+
+    void Awake()
+    {
+        textClock = GetComponent<TextMeshProUGUI>();
+        current = Time.time;
+        minusHealth = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DateTime time = DateTime.Now;
+        string hour = LeadingZero(time.Hour);
+        string minute = LeadingZero(time.Minute);
+        string second = LeadingZero(time.Second);
+        textClock.text = hour + ":" + minute + ":" + second;
+
+        if (Time.time - current >= secondsToMinusHealth && !minusHealth)
+        {
+            minusHealth = true;
+            current = Time.time;
+        }
+    }
+
+    string LeadingZero(int n)
+    {
+        return n.ToString().PadLeft(2, '0');
+    }
+}
---
+++ Assets/Scripts/Interaction/Energy System/Energy.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Energy : MonoBehaviour
+{
+    private TextMeshProUGUI textEnergy;
+    public int energyLevel;
+    GameObject clock;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        textEnergy = GetComponent<TextMeshProUGUI>();
+        energyLevel = 100;
+        textEnergy.text = energyLevel.ToString();
+        clock = GameObject.FindGameObjectWithTag("Clock");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (clock.GetComponent<Clock>().minusHealth) {
+            energyLevel -= 1;
+            textEnergy.text = energyLevel.ToString();
+            clock.GetComponent<Clock>().minusHealth = false;
+        }
+    }
+}
