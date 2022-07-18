95f3a03e063ba98f752a664c166fbc25ad69088e
Yu Hui <yuhui_lau@mymail.sutd.edu.sg>
2022-07-18 23:14:41 +0800

    feat: onclick change story panel

---
+++ Assets/Scripts/HUD/Story/State.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "State")]
+public class State : ScriptableObject
+{
+    [SerializeField] string gameText;
+    [SerializeField] State[] otherStates; //Cycle through states
+    
+    // Return game text for state
+    public string GetStateText()
+    {
+        return gameText;
+    }
+
+    // Update is called once per frame
+    public State[] GetOtherStates()
+    {
+        return otherStates;
+    }
+}
+    
+
